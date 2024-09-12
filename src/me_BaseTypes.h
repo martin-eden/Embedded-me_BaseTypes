60eb573cd7b879d0d5f031e4da62f31695950ef8
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-09-12 15:50:29 +0200

    [!-] Type declarations are removed from namespace..
    Okay, it's big conceptual change so I'm adding explanation why I removed
    base type declarations from namespace.
    
    Reason is pragmatism. Let's look at generations of some class
    with some boolean method:
    
    1 - Stock C++
    
      ```C++
        namespace me_Module
        {
          class TSomeClass
          {
            bool DoSomething();
          };
        }
      ```
    
      Okay, but I don't like stock C type nomenclature.
    
    2 - BaseTypes, earlier code
    
      ```C++
        #include <me_BaseTypes.h>
    
        namespace me_Module
        {
          using namespace me_BaseTypes;
    
          class TSomeClass
          {
            TBool DoSomething();
          };
        }
      ```
    
      Okay, but I'm bulk adding types to "me_Module" namespace.
    
    3 - BaseTypes, later code
    
      ```C++
        #include <me_BaseTypes.h>
    
        namespace me_Module
        {
          using
            me_BaseTypes::TBool;
    
          class TSomeClass
          {
            TBool DoSomething();
          };
        }
      ```
    
      Okay, but I'm adding shortcuts to needed types to "me_Module" namespace.
    
    4 - BaseTypes, latest code
    
      ```C++
        #include <me_BaseTypes.h>
    
        namespace me_Module
        {
          class TSomeClass
          {
            me_BaseTypes::TBool DoSomething();
          };
        }
      ```
    
      Okay, nothing non-essential is added to "me_Module" namespace.
      But source code is fucking polluted!
      Those bools and bytes are meat of any sensible code.
      Writing to me_BaseTypes::TUint_1 every time I need byte type
      is ugly, ugly, ugly!
    
    5 - BaseTypes, current code
    
      ```C++
        #include <me_BaseTypes.h>
    
        namespace me_Module
        {
          class TSomeClass
          {
            TBool DoSomething();
          };
        }
      ```
    
      Okay. Everyone should know what is TBool and where it is declared.
      All my current users are knowing this.
      And I'm not expecting big growth in "me_BaseTypes".
    
    Now I feel why all programming languages known to me don't wrap
    base types. It can be beautiful from mathematician point of view
    but it's not practical. Some basis should be preloaded.
    
    -- Martin, 2024-09-12

--- src/me_BaseTypes.h
+++ src/me_BaseTypes.h
@@ -1,36 +1,34 @@
 // My nomenclature of type names
 
 /*
-  Version: 4
-  Last mod.: 2024-06-27
+  Version: 5
+  Last mod.: 2024-09-12
 */
 
 #pragma once
 
-namespace me_BaseTypes
-{
-  typedef signed char TSint_1;
-  typedef signed int TSint_2;
-  typedef signed long int TSint_4;
-  typedef signed long long int TSint_8;
+typedef signed char TSint_1;
+typedef signed int TSint_2;
+typedef signed long int TSint_4;
+typedef signed long long int TSint_8;
 
-  typedef unsigned char TUint_1;
-  typedef unsigned int TUint_2;
-  typedef unsigned long int TUint_4;
-  typedef unsigned long long int TUint_8;
+typedef unsigned char TUint_1;
+typedef unsigned int TUint_2;
+typedef unsigned long int TUint_4;
+typedef unsigned long long int TUint_8;
 
-  typedef char TChar;
+typedef char TChar;
 
-  typedef bool TBool;
+typedef bool TBool;
 
-  typedef float TFloat;
+typedef float TFloat;
 
-  typedef void (*TMethod)(TUint_2 Data, TUint_2 State);
-}
+typedef void (*TMethod)(TUint_2 Data, TUint_2 State);
 
 /*
   2024-02-01
   2024-03-12
   2024-05-17 [+] Namespace, [-] <TBytes>
   2024-06-27 [+] TMethod
+  2024-09-12 [-] Namespace
 */
