9f6a06513141cd8d71982fbbd8fa4a9df08ad882
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-12-13 07:05:59 +0200

    [^/] Refinement changes.

--- src/me_BaseTypes.h
+++ src/me_BaseTypes.h
@@ -87,32 +87,28 @@ typedef TUint_2 TAddress;
 typedef bool TBool;
 
 /*
-  Signature for dumb class method
+  Method call
 
-  Method return's nothing, so it's dumb.
+  Three-address structure
 
-  But it needs data and it needs to know who he is.
-  In C++ methods are just free functions that are receiving
-  pointer to data structure of their class.
+  With dynamic functions generation we can reduce any call to
+  one address.
 
-  But it's not possible to get reference on method.
-  Only at this lower level when we know what we're doing.
-  Great language!
+  It's possible to reduce any call to two addresses but requires
+  packing data in instance.
 
-  Example we're mocking:
-
-    List.Add(Data);
+  Three addresses are quite convenient to call void methods.
 */
 typedef void (*TMethod)(TAddress Data, TAddress Instance);
 
 /*
-  Unit getter signature
+  Boolean method call
 
-  Example:
+  Three addresses plus boolean flag.
 
-    if (Flash.GetByte(&Value, FlashAddr)
+  More convenience for some processing tasks.
 */
-typedef TBool (*TUnitGetter)(TUnit * Value, TAddress Instance);
+typedef TBool (*TResponsiveMethod)(TAddress Data, TAddress Instance);
 
 /*
   2024-02-01
@@ -122,4 +118,5 @@ typedef TBool (*TUnitGetter)(TUnit * Value, TAddress Instance);
   2024-09-12 [-] Namespace
   2024-10-28 [-] TUint_8, TSint_8. No need
   2024-12-12 [+] TUnitGetter, [-] TChar, [+] TAsciiz
+  2024-12-13 [-] TUnitGetter, [+] TResponsiveMethod
 */
