54325dbc8b7f8af7eec9c59ff7948ece9c9dcbcc
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-08-27 15:18:02 +0200

    [!<] Moved function pointer types to [me_BaseInterfaces].

--- src/me_BaseTypes.h
+++ src/me_BaseTypes.h
@@ -1,7 +1,7 @@
 // My nomenclature of type names
 
 /*
-  Version: 11
+  Version: 12
   Author: Martin Eden
   Last mod.: 2025-08-25
 */
@@ -94,38 +94,8 @@ typedef TUint_2 TAddress;
 typedef bool TBool;
 
 /*
-  Method call
-
-  Three-address structure
-
-  With dynamic functions generation we can reduce any call to
-  one address.
-
-  It's possible to reduce any call to two addresses but requires
-  packing data in instance.
-
-  Three addresses are quite convenient to call void methods.
-*/
-typedef void (*TMethod)(TAddress Data, TAddress Instance);
-
-/*
-  Failable operation
-
-  Aka boolean method call.
-
-  More convenience for some processing tasks.
-*/
-typedef TBool (*TOperation)(TAddress Data, TAddress Instance);
-
-/*
-  Failable operation with one argument
-
-  Useful for stream input/output.
-*/
-typedef TBool (*TFixedOperation)(TAddress Data);
-
-/*
   2024 # # # # # # # # #
   2025-01-28
   2025-08-25
+  2025-08-27
 */
