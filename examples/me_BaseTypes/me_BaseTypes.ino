70197adb744f6ce996bca23e70e84231e209b97d
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-08-28 17:46:57 +0200

    [+] [Example] Compilation test now references all exported types.

--- examples/me_BaseTypes/me_BaseTypes.ino
+++ examples/me_BaseTypes/me_BaseTypes.ino
@@ -7,14 +7,41 @@
 
 #include <me_BaseTypes.h>
 
-void RunTest()
+void CompilationTest()
 {
-  [[gnu::unused]] const TUint_2 Limit = 10000;
+  [[gnu::unused]] TBool Bool;
+  [[gnu::unused]] TUint_1 Uint_1;
+  [[gnu::unused]] TUint_2 Uint_2;
+  [[gnu::unused]] TUint_4 Uint_4;
+  [[gnu::unused]] TSint_1 Sint_1;
+  [[gnu::unused]] TSint_2 Sint_2;
+  [[gnu::unused]] TSint_4 Sint_4;
+  [[gnu::unused]] TFloat Float;
+  [[gnu::unused]] TAsciiz Asciiz;
+
+  [[gnu::unused]] TUint_1 Uint_1_Max = TUint_1_Max;
+  [[gnu::unused]] TUint_2 Uint_2_Max = TUint_2_Max;
+  [[gnu::unused]] TUint_4 Uint_4_Max = TUint_4_Max;
+
+  [[gnu::unused]] TSint_1 Sint_1_Max = TSint_1_Max;
+  [[gnu::unused]] TSint_1 Sint_1_Min = TSint_1_Min;
+
+  [[gnu::unused]] TSint_2 Sint_2_Max = TSint_2_Max;
+  [[gnu::unused]] TSint_2 Sint_2_Min = TSint_2_Min;
+
+  [[gnu::unused]] TSint_4 Sint_4_Max = TSint_4_Max;
+  [[gnu::unused]] TSint_4 Sint_4_Min = TSint_4_Min;
+
+  [[gnu::unused]] TUnit Unit;
+  [[gnu::unused]] TAddress Address;
+
+  [[gnu::unused]] TAddressSegment AddressSegment =
+    { .Addr = 0, .Size = 0 };
 }
 
 void setup()
 {
-  RunTest();
+  CompilationTest();
 }
 
 void loop()
